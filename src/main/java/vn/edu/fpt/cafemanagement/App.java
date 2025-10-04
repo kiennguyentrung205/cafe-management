af1e241e40fec050ee444a5c0c88a50ab90fc815
Nguyen Trung Kien <kiennt.ce190036@gmail.com>
2025-10-05 00:55:44 +0700

    changed db name to "CafeDB1"

---
+++ src/main/java/vn/edu/fpt/cafemanagement/App.java
@@ -0,0 +1,10 @@
+package vn.edu.fpt.cafemanagement;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App {
+    public static void main(String[] args) {
+
+    }
+}
