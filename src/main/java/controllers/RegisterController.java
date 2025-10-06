70290e3309b9de81b547886884b5adfe6c913dad
MinhNhut190705 <NhutLM.CE190737@gmail.com>
2025-10-06 22:09:28 +0700

    add RegisterController

---
+++ src/main/java/controllers/RegisterController.java
@@ -0,0 +1,12 @@
+package controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class RegisterController {
+    @GetMapping(path = "/register")
+    public String showRegister(){
+        return "account/register";
+    }
+}
