46ed85033ca0878fa9582156a015658d7c829788
MinhNhut190705 <NhutLM.CE190737@gmail.com>
2025-10-07 21:11:37 +0700

    add login, table Controller

---
+++ src/main/java/vn/edu/fpt/cafemanagement/controllers/LoginController.java
@@ -0,0 +1,12 @@
+package vn.edu.fpt.cafemanagement.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+    @GetMapping
+    public String login() {
+        return "login";
+    }
+}
