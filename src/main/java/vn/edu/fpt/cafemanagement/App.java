8e1f3007a766b2d4ac5c5f10bb38a407370e0dd6
MinhNhut190705 <NhutLM.CE190737@gmail.com>
2025-10-07 10:28:38 +0700

    resolve conflict

--- src/main/java/vn/edu/fpt/cafemanagement/App.java
+++ src/main/java/vn/edu/fpt/cafemanagement/App.java
@@ -1,15 +1,21 @@
 package vn.edu.fpt.cafemanagement;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import vn.edu.fpt.cafemanagement.entities.Voucher;
+import vn.edu.fpt.cafemanagement.services.VoucherService;
+
+import java.util.List;
 
 @SpringBootApplication
 public class App {
     public static void main(String[] args) {
-        Dotenv dotenv = Dotenv.load();
-        System.setProperty("GOOGLE_CLIENT_ID", dotenv.get("GOOGLE_CLIENT_ID"));
-        System.setProperty("GOOGLE_CLIENT_SECRET", dotenv.get("GOOGLE_CLIENT_SECRET"));
-        SpringApplication.run(App.class, args);
+        ConfigurableApplicationContext context = SpringApplication.run(App.class, args);
+        VoucherService voucherService = context.getBean(VoucherService.class);
+        List<Voucher> list = voucherService.findAllByVoucherId(1);
+        for (Voucher voucher : list) {
+            System.out.println(String.format("Voucher Name: %s", voucher.getVoucherName()));
+        }
     }
 }
