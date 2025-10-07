94be5dcc608b69d3f9cb87acb821b1f2deb053b1
MinhNhut190705 <NhutLM.CE190737@gmail.com>
2025-10-07 10:34:56 +0700

    update env trong class main

--- src/main/java/vn/edu/fpt/cafemanagement/App.java
+++ src/main/java/vn/edu/fpt/cafemanagement/App.java
@@ -1,5 +1,6 @@
 package vn.edu.fpt.cafemanagement;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -17,5 +18,9 @@ public class App {
         for (Voucher voucher : list) {
             System.out.println(String.format("Voucher Name: %s", voucher.getVoucherName()));
         }
+
+        Dotenv dotenv = Dotenv.load();
+        System.setProperty("GOOGLE_CLIENT_ID", dotenv.get("GOOGLE_CLIENT_ID"));
+        System.setProperty("GOOGLE_CLIENT_SECRET", dotenv.get("GOOGLE_CLIENT_SECRET"));
     }
 }
