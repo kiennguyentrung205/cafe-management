0cf793c6700337a7dedcb14c8e5b5cbcfc07f715
MinhNhut190705 <NhutLM.CE190737@gmail.com>
2025-10-07 11:01:10 +0700

    update env trong class main

--- src/main/java/vn/edu/fpt/cafemanagement/App.java
+++ src/main/java/vn/edu/fpt/cafemanagement/App.java
@@ -13,11 +13,11 @@ import java.util.List;
 public class App {
     public static void main(String[] args) {
         ConfigurableApplicationContext context = SpringApplication.run(App.class, args);
-        VoucherService voucherService = context.getBean(VoucherService.class);
-        List<Voucher> list = voucherService.findAllByVoucherId(1);
-        for (Voucher voucher : list) {
-            System.out.println(String.format("Voucher Name: %s", voucher.getVoucherName()));
-        }
+//        VoucherService voucherService = context.getBean(VoucherService.class);
+//        List<Voucher> list = voucherService.findAllByVoucherId(1);
+//        for (Voucher voucher : list) {
+//            System.out.println(String.format("Voucher Name: %s", voucher.getVoucherName()));
+//        }
 
         Dotenv dotenv = Dotenv.load();
         System.setProperty("GOOGLE_CLIENT_ID", dotenv.get("GOOGLE_CLIENT_ID"));
