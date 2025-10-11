9ceb0f66bbf24aad1951e98aa1ab1a4287fe55f3
Nguyen Tan Quoc <QuocNT.CE190614@gmail.com>
2025-10-11 22:12:28 +0700

    thêm sửa xóa view product đã ổn rồi ạ. có cái category mới thêm cái thuộc tính là is_active tại vì phải crud cái category nữa á

--- src/main/java/vn/edu/fpt/cafemanagement/repositories/CategoryRepository.java
+++ src/main/java/vn/edu/fpt/cafemanagement/repositories/CategoryRepository.java
@@ -10,4 +10,6 @@ import java.util.List;
 public interface CategoryRepository extends JpaRepository<Category, Integer> {
     public List<Category> findCategoriesByCateNameContainingIgnoreCase(String cateName);
 
+    List<Category> findByIsActiveTrue();
+
 }
