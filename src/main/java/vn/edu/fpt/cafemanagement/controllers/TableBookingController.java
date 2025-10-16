88398db835aa01cea51224503e6bb0a9eb8080fd
MinhNhut190705 <NhutLM.CE190737@gmail.com>
2025-10-16 13:24:42 +0700

    update Table Booking

--- src/main/java/vn/edu/fpt/cafemanagement/controllers/TableBookingController.java
+++ src/main/java/vn/edu/fpt/cafemanagement/controllers/TableBookingController.java
@@ -3,6 +3,7 @@ package vn.edu.fpt.cafemanagement.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import vn.edu.fpt.cafemanagement.entities.Customer;
 import vn.edu.fpt.cafemanagement.entities.Table;
 import vn.edu.fpt.cafemanagement.entities.TableBooking;
@@ -11,6 +12,10 @@ import vn.edu.fpt.cafemanagement.services.CustomerService;
 import vn.edu.fpt.cafemanagement.services.TableBookingService;
 import vn.edu.fpt.cafemanagement.services.TableService;
 
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Date;
 import java.util.List;
 
 @Controller
@@ -46,6 +51,10 @@ public class TableBookingController {
     @GetMapping(path = "/new")
     public String showBookingTable(Model model, @RequestParam("table-id") int tableId) {
 
+        if (model.containsAttribute("errorMessage")) {
+            System.out.println("🔥 Có lỗi: " + model.getAttribute("errorMessage"));
+        }
+
         Customer loggedCustomer = loggedUser.getLoggedCustomer();
         Table table = tableService.findById(tableId);
         TableBooking tableBooking = new TableBooking();
@@ -53,6 +62,7 @@ public class TableBookingController {
 
         tableBooking.setCustomer(loggedCustomer);
 
+        model.addAttribute("now", LocalDate.now());
         model.addAttribute("tableBooking", tableBooking);
         model.addAttribute("table", table);
         model.addAttribute("loggedCustomer", loggedCustomer);
@@ -61,12 +71,36 @@ public class TableBookingController {
     }
 
     @PostMapping(path="/new")
-    public String addBooking(Model model, @ModelAttribute TableBooking tableBooking) {
+    public String addBooking(Model model, @ModelAttribute TableBooking tableBooking, RedirectAttributes redirectAttributes) {
         Customer loggedCustomer = loggedUser.getLoggedCustomer();
         tableBooking.setCustomer(loggedCustomer);
 
+        LocalDateTime bookingTime = tableBooking.getBookingTime();
+        LocalDateTime now = LocalDateTime.now();
+
+
+// 2️⃣ Chỉ được đặt trong vòng 2 tiếng trước khi tới
+        long diffMinutes = Duration.between(now, bookingTime).toMinutes();
+        if (diffMinutes < 0) {
+            redirectAttributes.addFlashAttribute("errorMessage", "You cannot book table in the past!");
+            return "redirect:/table/booking/new?table-id=" + tableBooking.getTable().getTableId();
+        }
+
+        if (diffMinutes > 120) {
+            redirectAttributes.addFlashAttribute("errorMessage", "You can only book a table within 2 hours before your arrival");
+            return "redirect:/table/booking/new?table-id=" + tableBooking.getTable().getTableId();
+        }
+
+// 3️⃣ Không được đặt sau 22 giờ
+        if (bookingTime.getHour() >= 22) {
+            redirectAttributes.addFlashAttribute("errorMessage", "You cannot book table after 22:00!");
+            return "redirect:/table/booking/new?table-id=" + tableBooking.getTable().getTableId();
+        }
+
         tableBookingService.save(tableBooking);
         tableService.updateStatus(tableBooking.getTable().getTableId(), "unavailable");
+
+        redirectAttributes.addFlashAttribute("successMessage", "Table booking has been saved successfully!");
 
         return "redirect:/table/list?book-success";
     }
