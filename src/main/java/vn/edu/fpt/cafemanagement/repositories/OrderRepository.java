7c0bc39b56212ff874f1962813233fb509ad21db
Nguyen Trung Kien <kiennt.ce190036@gmail.com>
2025-10-23 11:37:28 +0700

    Sales Management: View Sale, View Sale by Period,Export to Excel

--- src/main/java/vn/edu/fpt/cafemanagement/repositories/OrderRepository.java
+++ src/main/java/vn/edu/fpt/cafemanagement/repositories/OrderRepository.java
@@ -4,16 +4,27 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import vn.edu.fpt.cafemanagement.entities.Order;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Repository
-public interface  OrderRepository extends JpaRepository<Order, Integer> {
+public interface OrderRepository extends JpaRepository<Order, Integer> {
 
     List<Order> findByCustomerCusId(int cusId);
 
     @Query("SELECT o FROM Order o WHERE o.isActive = false ORDER BY o.createdAt DESC")
     Page<Order> findActiveOrders(Pageable pageable);
+
+    @Query("SELECT COALESCE(SUM(o.totalPrice), 0), COUNT(o), COUNT(o.voucher) " +
+            "FROM Order o " +
+            "WHERE o.createdAt >= :startDate AND o.createdAt < :endDate")
+    List<Object[]> getSalesSummaryObject(
+            @Param("startDate") LocalDateTime startDate,
+            @Param("endDate") LocalDateTime endDate);
+
+    List<Order> findAllByCreatedAtGreaterThanEqualAndCreatedAtLessThan(LocalDateTime startDate, LocalDateTime endDate);
 }
