c5f33024cec0b41525dea9c2c4b995269a01b788
tainguyenhuutai2005 <tainh.ce190284@gmail.com>
2025-11-13 03:23:59 +0700

    add order history page for customer, update voucher usage, and add max points button for create order

--- src/main/java/vn/edu/fpt/cafemanagement/repositories/VoucherRepository.java
+++ src/main/java/vn/edu/fpt/cafemanagement/repositories/VoucherRepository.java
@@ -1,9 +1,12 @@
 package vn.edu.fpt.cafemanagement.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import vn.edu.fpt.cafemanagement.entities.Voucher;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Repository
@@ -14,4 +17,10 @@ public interface VoucherRepository extends JpaRepository<Voucher, Integer> {
     Voucher findByVoucherId(int voucherId);
 
     void deleteByVoucherId(int voucherId);
+
+    @Query("SELECT v FROM Voucher v WHERE v.isActive = true " +
+            "AND v.quantity > 0 " +
+            "AND v.startDate <= :currentDate " +
+            "AND v.endDate >= :currentDate")
+    List<Voucher> findApplicableVouchers(@Param("currentDate") LocalDate currentDate);
 }
